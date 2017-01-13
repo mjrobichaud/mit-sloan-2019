9a827414d16357ed516631ed7df1e7d0a74c8a21
Michael Robichaud <robi@Michaels-MacBook-Pro.local>
2017-01-13 15:31:14 +0000

    minify some files for performance

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,26 @@ var gulp = require('gulp'),
   gp_cssmin = require('gulp-minify-css'),
   gp_uglify = require('gulp-uglify');
 
-gulp.task('src-min', function(){
-    return gulp.src(['vendor/jquery/jquery.js', 'vendor/bootstrap/js/bootstrap.js', 'js/new-age.js'])
-        .pipe(gp_rconcat({extname: ".js", outside: true}))
-        .pipe(gp_annotate())
+gulp.task('js-min', function(){
+    return gulp.src(['vendor/jquery/jquery.js', 'vendor/bootstrap/js/bootstrap.js', 'vendor/easing/jquery.easing.js', 'js/new-age.js'])
+        //.pipe(gp_rconcat({extname: ".js", outside: true}))
+        .pipe(gp_concat('all.js'))
         .pipe(gulp.dest('dist'))
-        .pipe(gp_rename('src-min.js'))
+        .pipe(gp_rename('js-min.js'))
         .pipe(gp_uglify())
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('css-min', function() {
+    return gulp.src([
+        'vendor/bootstrap/css/bootstrap.css',
+        'vendor/font-awesome/css/font-awesome.css',
+        'css/new-age.css'])
+        .pipe(gp_concat('all.css'))
+        .pipe(gulp.dest('dist'))
+        .pipe(gp_rename('all.min.css'))
+        .pipe(gp_cssmin())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js-min', 'css-min'], function(){});

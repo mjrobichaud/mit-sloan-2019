daad1ffe3c79faddc95a96ad148e076f44b78f1c
Michael Robichaud <robi@Michaels-MacBook-Pro.local>
2017-01-16 22:10:39 +0000

    final commit, ready to submit!

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # mit-sloan-2019
 Optional Information for my application to the MIT Sloan MBA class of 2019.
+
+## Background
+I chose to make a website because I thought it would be a good opportunity to showcase my web development skills and is a creative medium to explain my message.
+
+## Technology
+I started with a template from https://startbootstrap.com to save some time. Other libraries and technologies used:
+- Bootstrap
+- Fontawesome
+- Google Fonts
+- Jquery
+
+## Hosting
+The website is hosted as a static website using Amazon S3. 
+
+## Fallback
+The file error.pdf contains a PDF version of the website as an alternative way to view the content in the event of errors or issues with s3.
+
--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,42 @@ var gulp = require('gulp'),
   gp_uglify = require('gulp-uglify');
 
 gulp.task('js-min', function(){
-    return gulp.src(['vendor/jquery/jquery.js', 'vendor/bootstrap/js/bootstrap.js', 'vendor/easing/jquery.easing.js', 'js/new-age.js'])
-        //.pipe(gp_rconcat({extname: ".js", outside: true}))
-        .pipe(gp_concat('all.js'))
-        .pipe(gulp.dest('dist'))
-        .pipe(gp_rename('js-min.js'))
-        .pipe(gp_uglify())
-        .pipe(gulp.dest('dist'));
+  return gulp.src(['vendor/jquery/jquery.js', 'vendor/bootstrap/js/bootstrap.js', 'vendor/easing/jquery.easing.js', 'js/new-age.js'])
+    .pipe(gp_concat('all.js'))
+    .pipe(gulp.dest('js'))
+    .pipe(gp_rename('js-min.js'))
+    .pipe(gp_uglify())
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('css-min', function() {
-    return gulp.src([
-        'vendor/bootstrap/css/bootstrap.css',
-        'vendor/font-awesome/css/font-awesome.css',
-        'css/new-age.css'])
-        .pipe(gp_concat('all.css'))
-        .pipe(gulp.dest('dist'))
-        .pipe(gp_rename('all.min.css'))
-        .pipe(gp_cssmin())
-        .pipe(gulp.dest('dist'));
+  return gulp.src([
+    'vendor/bootstrap/css/bootstrap.css',
+    'vendor/font-awesome/css/font-awesome.css',
+    'css/new-age.css'])
+    .pipe(gp_concat('all.css'))
+    .pipe(gulp.dest('css'))
+    .pipe(gp_rename('all.min.css'))
+    .pipe(gp_cssmin())
+    .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('default', ['js-min', 'css-min'], function(){});
+gulp.task('img', function() {
+  return gulp.src([
+    'img/chart.png',
+    'img/gears.png',
+    'img/lightbulb.png',
+    'img/bg2.jpg',
+    'img/bg4.jpg',
+    'img/headshot.jpg',
+    'img/bg-pattern.png'])
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('index', function() {
+  return gulp.src([
+    'index.html'])
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js-min', 'css-min', 'img', 'index'], function(){});
